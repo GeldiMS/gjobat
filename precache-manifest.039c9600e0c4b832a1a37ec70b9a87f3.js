017ae4e6e03aa2fdf2128cc5cc1509c414985542
GeldiMS <mansakugeldi@gmail.com>
2020-06-25 22:28:50 +0200

    Updates

---
+++ precache-manifest.039c9600e0c4b832a1a37ec70b9a87f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998e5362d456df94118e3aa5b4840bc6",
+    "url": "/gjobat/index.html"
+  },
+  {
+    "revision": "9b4c7c5f18b97d7f1140",
+    "url": "/gjobat/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e05be862deeb205493d3",
+    "url": "/gjobat/static/js/2.eecbb7cd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gjobat/static/js/2.eecbb7cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4c7c5f18b97d7f1140",
+    "url": "/gjobat/static/js/main.56f85feb.chunk.js"
+  },
+  {
+    "revision": "32acd6e7eddd3f6f6208",
+    "url": "/gjobat/static/js/runtime-main.0a3da88e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gjobat/static/media/logo.5d5d9eef.svg"
+  }
+]);
