3ed0c180de814503b65abc3ac4cfb2c4d8d51f9d
GeldiMS <mansakugeldi@gmail.com>
2020-06-25 21:53:00 +0200

    feat/init-gjobat-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { Button } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Gjobat extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      targa: '', 
+      shasia: '',
+      isSubmitted: false,
+      message: 'Kerkesa juaj po procedohet...',
+    };
+  }
+
+  handleChange = event => {
+    const value = event.target.value;
+    this.setState({
+      [event.target.name]: value,
+    })
+  }
+
+  handleSubmit = (event) => {
+    this.setState({
+      isSubmitted: true
+    })
+    axios.post('http://c939c1863fd5.ngrok.io', {
+      targa: this.state.targa,
+       shasia: this.state.shasia
+      })
+    .then(response => {
+      this.setState({message: response.data.message})
+    });
+  
+    // axios({
+    //   method: 'get',
+    //   url: 'http://localhost:5000',
+    // })
+    //   .then(res => {
+    //   this.setState({
+    //     message: res.data.message
+    //   })
+    // });
+    event.preventDefault();
 }
 
-export default App;
+render() {
+  if(!this.state.isSubmitted){
+    console.log(this.state.isSubmitted)
+  return (
+    <form onSubmit={this.handleSubmit}>
+      <label>
+        Targa:
+        <input type="text" value={this.state.value} name="targa" onChange={this.handleChange} />
+        Shasia:
+        <input type="text" value={this.state.value} name="shasia" onChange={this.handleChange} />
+        <h1>{this.state.isSubmitted}</h1>
+      </label>
+      <Button onClick={this.handleSubmit}>Submit</Button>
+    </form>
+  );
+  }
+  else{
+    return(
+      <h1>{this.state.message}</h1>
+    )
+  }
+}
+}
